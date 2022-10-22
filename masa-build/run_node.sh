1c66f952d463bee7c17ac0f902037046af9ba2d4
MichaelTMS <terentevms@gmail.com>
2022-10-22 11:46:39 +0300

    Fix masa

--- masa-build/run_node.sh
+++ masa-build/run_node.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
 BASEDIR="/app"
-CONFIGDIR="/tmp/conf"
+CONFIGDIR_TMP="/tmp/conf"
 GENESIS_FILE="$BASEDIR/genesis.json"
 NODE_ID=${NODE_NAME:="Masa$(uname)"}
+VERBOSITY=${LOGGING:=3}
 BOOTNODES="enode://7612454dd41a6d13138b565a9e14a35bef4804204d92e751cfe2625648666b703525d821f34ffc198fac0d669a12d5f47e7cf15de4ebe65f39822a2523a576c4@81.29.137.40:30300"
 
 if [[ ! -f $GENESIS_FILE ]]
 then
-    NETWORK_ID=$(cat ${CONFIGDIR}/genesis.json | grep chainId | awk -F " " '{print $2}' | awk -F "," '{print $1}')
+    NETWORK_ID=$(cat ${CONFIGDIR_TMP}/genesis.json | grep chainId | awk -F " " '{print $2}' | awk -F "," '{print $1}')
 else
     NETWORK_ID=$(cat ${GENESIS_FILE} | grep chainId | awk -F " " '{print $2}' | awk -F "," '{print $1}')
 fi
 
 init_node() {
     mkdir -p $BASEDIR/{keystore,geth}
-    cp -rf $CONFIGDIR/* $BASEDIR 
+    cp -rf $CONFIGDIR_TMP/* $BASEDIR 
     geth --datadir $BASEDIR init $GENESIS_FILE
     echo "Created $(date)" > $BASEDIR/control_file
-    rm -rf $CONFIGDIR
+    rm -rf $CONFIGDIR_TMP
 }
 
 start_node() {
@@ -31,8 +32,8 @@ start_node() {
     --istanbul.blockperiod 10 \
     --mine \
     --miner.threads 1 \
-    --syncmode full
-    --verbosity 5 \
+    --syncmode full \
+    --verbosity $VERBOSITY \
     --http \
     --http.corsdomain "*" \
     --http.vhosts "*" \
@@ -42,7 +43,7 @@ start_node() {
     --port 21000
 }
 
-if [[ $LOGGING="true" ]]
+if [[ $LOGGING -ge 4 ]]
 then
     set -x
 fi
@@ -52,10 +53,10 @@ if [ ! -f "$BASEDIR/control_file" ]; then
     init_node
 fi
 
-if [[ -d $CONFIGDIR && $(ls $CONFIGDIR | wc -l) -gt 0 ]]
+if [[ -d $CONFIGDIR_TMP && $(ls $CONFIGDIR_TMP | wc -l) -gt 0 ]]
 then
-    cp -rf $CONFIGDIR/* $BASEDIR 
-    rm -rf $CONFIGDIR
+    cp -rf $CONFIGDIR_TMP/* $BASEDIR 
+    rm -rf $CONFIGDIR_TMP
 fi
 
 echo "Node launch"

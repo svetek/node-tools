75c2181707cbae4cf273102c82da4e9dcde9064b
MichaelTMS <terentevms@gmail.com>
2023-09-07 20:31:37 +0300

    [Massa] Update build scripts

--- massa-node/run_node.sh
+++ massa-node/run_node.sh
@@ -4,28 +4,28 @@ BASEDIR="/app"
 SOURCE_DIR="/source"
 MN_WORKDIR="$BASEDIR/massa-node"
 MC_WORKDIR="$BASEDIR/massa-client"
-PRIVKEY="$MN_WORKDIR/config/node_privkey.key"
-WALLET="$MN_WORKDIR/config/staking_wallet.dat"
+# PRIVKEY="$MN_WORKDIR/config/node_privkey.key"
+# WALLET="$MN_WORKDIR/config/staking_wallet.dat" - not use
 
-init_node() {
-    cd $MN_WORKDIR
-    expect -c "
-        #!/usr/bin/expect -f
-        set timeout -1
+# init_node() {
+#     cd $MN_WORKDIR
+#     expect -c "
+#         #!/usr/bin/expect -f
+#         set timeout -1
 
-        spawn ./massa-node
-        exp_internal 0
-        expect \"Enter new password for staking keys file:\"
-        send   \"$PASSWORD\n\"
-        expect \"Confirm password:\"
-        send   \"$PASSWORD\n\"
-        expect eof
-    "
-}
+#         spawn ./massa-node
+#         exp_internal 0
+#         expect \"Enter new password for staking keys file:\"
+#         send   \"$PASSWORD\n\"
+#         expect \"Confirm password:\"
+#         send   \"$PASSWORD\n\"
+#         expect eof
+#     "
+# }
 
-start_node() {
-    cd $MN_WORKDIR && ./massa-node -p $PASSWORD
-}
+# start_node() {
+#     cd $MN_WORKDIR && ./massa-node -p $PASSWORD
+# }
 
 if [[ $LOGGING == "true" ]]
 then
@@ -36,33 +36,39 @@ fi
 if [[ $(ls $SOURCE_DIR | wc -l) -gt 0 ]]
 then
     if [[ ! -d $MC_WORKDIR || ! -d $MN_WORKDIR ]]
-    then 
-        echo "Node and client installation"
+    then
         mkdir -p $MC_WORKDIR $MN_WORKDIR
         cp -rf $SOURCE_DIR/massa-client/* $MC_WORKDIR
         cp -rf $SOURCE_DIR/massa-node/* $MN_WORKDIR
-        cp -f $SOURCE_DIR/version $BASEDIR
+        cp -f $SOURCE_DIR/version.json $BASEDIR
+
+        echo -e "\n\e[32m### The Mass Node and Massa Client installation is complete. ###\e[0m\n"
+        echo "1) $(${MN_WORKDIR}/massa-node -V) ver."
+        echo "2) $(${MC_WORKDIR}/massa-client -V) ver."
     else
-        echo "Node and client update"
-        ver_old=$(cat $BASEDIR/version 2>/dev/null | jq -r .version)
+        ver_old=$(cat $BASEDIR/version.json 2>/dev/null | jq -r .version)
         ver=${ver_old:="unknown.bak"}
         cp -f $SOURCE_DIR/massa-client/massa-client $MC_WORKDIR
         cp -f $SOURCE_DIR/massa-node/massa-node $MN_WORKDIR
         cp -fbr -S "-$ver.bak" $SOURCE_DIR/massa-client/{config,base_config} $MC_WORKDIR
         cp -fbr -S "-$ver.bak" $SOURCE_DIR/massa-node/{config,base_config} $MN_WORKDIR
-        cp -f $SOURCE_DIR/version $BASEDIR
+        cp -f $SOURCE_DIR/version.json $BASEDIR
+        echo -e "\n\e[32m### The Mass Node and Massa Client update is complete. ###\e[0m\n"
+        echo "1) $(${MN_WORKDIR}/massa-node -V) ver."
+        echo "2) $(${MC_WORKDIR}/massa-client -V) ver."
     fi
     echo -e "[network]\nroutable_ip = \"`wget -qO- eth0.me`\"" > $MN_WORKDIR/config/config.toml
     echo 'alias massa-client="cd /app/massa-client && ./massa-client -p $PASSWORD"' >> $HOME/.bashrc
     rm -rf $SOURCE_DIR/*
 fi
 
-## Massa node launch ###
-if [[ ! -f $PRIVKEY || ! -f $WALLET ]]
-then
-    echo "Node initialization"
-    init_node
-fi
+# ## Massa node launch ###
+# if [[ ! -f $PRIVKEY ]]
+# then
+#     echo -e "\n\e[32m### The Massa Node initialization ###\e[0m\n"
+#     init_node
+# fi
 
-echo "Node launch"
-start_node
+echo -e "\n\e[32m### The Massa Node launch ###\e[0m\n"
+cd $MN_WORKDIR && ./massa-node -p $PASSWORD
+# start_node

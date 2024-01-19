f0599b92661d6d7130ab5c272b98834c0e61f9db
Mikhail Terentev <michael@svetek.com>
2024-01-19 17:28:08 +0300

    [Osmosis] Update build scripts

--- osmosis/build.sh
+++ osmosis/build.sh
@@ -3,23 +3,11 @@ export DOCKER_BUILDKIT=1
 
 set -e
 
+BIN="osmosisd"
+BUILD_DATE="$(date -u +'%Y-%m-%d')"
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 DOCKERFILE="$DIR/Dockerfile"
-BUILD_DATE="$(date -u +'%Y-%m-%d')"
 GIT_REPOSITORY=https://github.com/osmosis-labs/osmosis
-
-NODE_TYPES=("node")
-
-echo "What node type is required for build?"
-PS3="Node type selected: "
-select node_type in "${NODE_TYPES[@]}"
-do
-    case $node_type in
-        "node")
-            BIN="artelad"; break
-            ;;
-    esac
-done
 
 read -p "Enter image name: " -r IMAGE_NAME
 read -p "Enter release tag: " -r IMAGE_TAG
@@ -42,9 +30,9 @@ if [[ "$PUSH_FLAG" == "yes" ]]
 then
     read -r -p "Enter username: " DOCKERHUB_USERNAME
     read -r -p "Enter password: " DOCKERHUB_PASSWORD
-    IMAGE=$DOCKERHUB_USERNAME/$IMAGE_NAME:$IMAGE_TAG-$node_type
+    IMAGE=$DOCKERHUB_USERNAME/$IMAGE_NAME:$IMAGE_TAG
 else
-    IMAGE=$IMAGE_NAME:$IMAGE_TAG-$node_type
+    IMAGE=$IMAGE_NAME:$IMAGE_TAG
 fi
 
 echo -e "\n\e[32m### The build information ###\e[0m"
@@ -52,14 +40,12 @@ echo -e "Build date: \t$BUILD_DATE"
 echo -e "Docker context: $DIR"
 echo -e "Dockerfile: \t$DOCKERFILE"
 echo -e "Docker Image: \t$IMAGE"
-echo -e "Node type: \t$node_type"
 echo -e "Version: \t$IMAGE_TAG\n"
 
 docker build -f "$DOCKERFILE" "$DIR" \
      --build-arg IMAGE_TAG="$IMAGE_TAG" \
      --build-arg GIT_REPOSITORY="$GIT_REPOSITORY" \
      --build-arg BIN="$BIN" \
-     --build-arg NODE_TYPE="$node_type" \
      --tag $IMAGE
 
 if [[ "$PUSH_FLAG" == "yes" ]]

a05eb5448f244fea73ec6828a80c76b185ba17e3
Mikhail Terentev <michael@svetek.com>
2024-03-20 22:03:06 +0000

    [Lava] Update the run script

--- lava/run_node.sh
+++ lava/run_node.sh
@@ -28,19 +28,20 @@ init_node() {
     -e 's|^filter_peers =.*|filter_peers = "true"|' \
     -e "s|^persistent_peers =.*|persistent_peers = \"$PEERS\"|" \
     -e "s|^seeds =.*|seeds = \"$SEEDS\"|" \
-    -e 's|^create_empty_blocks =.*|create_empty_blocks = true|' \
-    -e 's|^create_empty_blocks_interval =.*|create_empty_blocks_interval = "60s"|' \
     $CONFIG_PATH/config/config.toml
 
   # Set timeout
   sed -i \
-    -e 's|^timeout_commit =.*|timeout_commit = "30s"|' \
     -e 's|^timeout_propose =.*|timeout_propose = "1s"|' \
-    -e 's|^timeout_precommit =.*|timeout_precommit = "1s"|' \
-    -e 's|^timeout_precommit_delta =.*|timeout_precommit_delta = "500ms"|' \
+    -e 's|^timeout_propose_delta =.*|timeout_propose_delta = "500ms"|' \
     -e 's|^timeout_prevote =.*|timeout_prevote = "1s"|' \
     -e 's|^timeout_prevote_delta =.*|timeout_prevote_delta = "500ms"|' \
-    -e 's|^timeout_propose_delta =.*|timeout_propose_delta = "500ms"|' \
+    -e 's|^timeout_precommit =.*|timeout_precommit = "500ms"|' \
+    -e 's|^timeout_precommit_delta =.*|timeout_precommit_delta = "1s"|' \
+    -e 's|^timeout_commit =.*|timeout_commit = "15s"|' \
+    -e 's|^create_empty_blocks =.*|create_empty_blocks = true|' \
+    -e 's|^create_empty_blocks_interval =.*|create_empty_blocks_interval = "15s"|' \
+    -e 's|^timeout_broadcast_tx_commit =.*|timeout_broadcast_tx_commit = "151s"|' \
     -e 's|^skip_timeout_commit =.*|skip_timeout_commit = false|' \
     $CONFIG_PATH/config/config.toml
 

c583b86d22c996dfd0e86b11d346ae5f36a1e2cc
MichaelS <michael@svetek.com>
2024-08-19 13:37:16 +0000

    [Monitoring] Update metrics script

--- monitoring/scripts/lava_metrics.py
+++ monitoring/scripts/lava_metrics.py
@@ -30,12 +30,13 @@ class MetricsCollector:
 
     def get_chain_status(self):
         metrics = ['# HELP Lava provider chain status.', '# TYPE lava_provider_chain_status gauge']
+        moniker_label = f', moniker="{self.moniker}"' if self.moniker else ''
         statuses = ['provider', 'frozen', 'unstaked']
         output = json.loads(self.run_command(f"docker exec {self.container} bash -c 'lavap query pairing account-info --from $WALLET -o json'"))
         for status in statuses:
             for item in output.get(status, []):
                 jail_end_time=datetime.fromtimestamp(int(item["jail_end_time"])).strftime("%Y-%m-%d %H:%M:%S") if int(item.get("jail_end_time", "0")) > 0 else 0
-                metrics.append(f'lava_provider_chain_status{{network="{self.network}", moniker="{item.get("moniker", "")}", chainID="{item["chain"]}", status="{status}", jails="{item["jails"]}", jail_end_time="{jail_end_time}"}} 1')
+                metrics.append(f'lava_provider_chain_status{{network="{self.network}", chainID="{item["chain"]}", status="{status}", jails="{item["jails"]}", jail_end_time="{jail_end_time}" {moniker_label}}} 1')
         self.write_metrics_to_file(metrics, 'lava_provider_chain_status')
 
     def write_metrics_to_file(self, metrics, metric_name):

3b0eff497cc8b5c7b5917841b3573dd12204797b
Mikhail Terentev <mterentev@enfint.ai>
2026-05-25 21:16:38 +0400

    [cosmos] add rocksdb support

--- cosmos/build.sh
+++ cosmos/build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 export DOCKER_BUILDKIT=1
 
-set -e
+set -euo pipefail
 
-BIN="gaiad"
 BUILD_DATE="$(date -u +'%Y-%m-%d')"
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 DOCKERFILE="$DIR/Dockerfile"
-GIT_REPOSITORY=https://github.com/cosmos/gaia
 
-read -p "Enter image name: " -r IMAGE_NAME
-read -p "Enter release tag: " -r IMAGE_TAG
+read -r -p "Enter image name: " IMAGE_NAME
+read -r -p "Enter release tag: " RELEASE_TAG
+
+IMAGE_TAG="$RELEASE_TAG"
 
 echo "Do you want to send the image to DockerHub?"
 PS3="Send the image to DockerHub: "
@@ -29,10 +29,11 @@ done
 if [[ "$PUSH_FLAG" == "yes" ]]
 then
     read -r -p "Enter username: " DOCKERHUB_USERNAME
-    read -r -p "Enter password: " DOCKERHUB_PASSWORD
-    IMAGE=$DOCKERHUB_USERNAME/$IMAGE_NAME:$IMAGE_TAG
+    read -r -s -p "Enter password: " DOCKERHUB_PASSWORD
+    echo
+    IMAGE="$DOCKERHUB_USERNAME/$IMAGE_NAME:$IMAGE_TAG"
 else
-    IMAGE=$IMAGE_NAME:$IMAGE_TAG
+    IMAGE="$IMAGE_NAME:$IMAGE_TAG"
 fi
 
 echo -e "\n\e[32m### The build information ###\e[0m"
@@ -40,19 +41,18 @@ echo -e "Build date: \t$BUILD_DATE"
 echo -e "Docker context: $DIR"
 echo -e "Dockerfile: \t$DOCKERFILE"
 echo -e "Docker Image: \t$IMAGE"
-echo -e "Version: \t$IMAGE_TAG\n"
+echo -e "Release tag: \t$RELEASE_TAG"
+echo -e "Image tag: \t$IMAGE_TAG\n"
 
 docker build -f "$DOCKERFILE" "$DIR" \
-     --build-arg IMAGE_TAG="$IMAGE_TAG" \
-     --build-arg GIT_REPOSITORY="$GIT_REPOSITORY" \
-     --build-arg BIN="$BIN" \
-     --tag $IMAGE
+     --build-arg GAIA_VERSION="$RELEASE_TAG" \
+     --tag "$IMAGE"
 
 if [[ "$PUSH_FLAG" == "yes" ]]
 then
     echo -e "\nSending docker image \"$IMAGE\" to DockerHub\n"
-    docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_PASSWORD 2>/dev/null
-    docker push $IMAGE
+    docker login -u "$DOCKERHUB_USERNAME" -p "$DOCKERHUB_PASSWORD" 2>/dev/null
+    docker push "$IMAGE"
 fi
 
 echo -e "\n\e[32mThe build is complete! \n\e[0m"
